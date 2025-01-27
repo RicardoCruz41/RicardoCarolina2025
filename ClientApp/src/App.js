96891f272ade6693463098f2e144028da6d874c2
Ricardo Cruz <Ricardo_Cruz_41@hotmail.com>
2025-01-27 00:00:32 +0000

    - add endpoint change

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -30,7 +30,7 @@ const App = ({ switchLanguage, locale }) => {
             <br />
             <TimeAndLocation />
             <br />
-            <AttendanceForm apiEndpoint="https://ricardocarolina2025-efdpfjcwf2g3aqe4.westeurope-01.azurewebsites.net/Attendance/SendAttendanceConfirmation" currentLanguage={locale} />
+            <AttendanceForm apiEndpoint="http://weddingapi-imp9.onrender.com/api/attendance/SendAttendanceConfirmation" currentLanguage={locale} />
             <br />
           <GiftDetails/>
     </div>
